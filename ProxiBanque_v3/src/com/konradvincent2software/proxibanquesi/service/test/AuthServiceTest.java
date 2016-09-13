0ab91680c3886b0eeee8f663c8f18fc676b414e3
Vincent PANOUILLERES <panouilleres.vincent@yahoo.fr>
2016-09-13 04:16:55 +0200

    Test Junit Authentification ok

--- ProxiBanque_v3/src/com/konradvincent2software/proxibanquesi/service/test/AuthServiceTest.java
+++ ProxiBanque_v3/src/com/konradvincent2software/proxibanquesi/service/test/AuthServiceTest.java
@@ -1,15 +1,26 @@
 package com.konradvincent2software.proxibanquesi.service.test;
 
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-import com.konradvincent2software.proxibanquesi.service.AuthService;;
+import com.konradvincent2software.proxibanquesi.domaine.Conseiller;
+import com.konradvincent2software.proxibanquesi.service.AuthService;
+import com.konradvincent2software.proxibanquesi.service.ConseillerService;;
 
 public class AuthServiceTest {
 
+	@BeforeClass
+	public static void avantTest(){
+		Conseiller conseiller = new Conseiller("Robert", "Dupond", "Monsieur", "rdupond", "gtm", null);
+		ConseillerService service = new ConseillerService();
+		service.creerConseiller(conseiller);
+	}
+	
 	@Test
 	public void testAuthConseiller() {
-		String login = "ccastro";
+		String login = "rdupond";
 		String pwd = "gtm";
 		boolean status;
 
@@ -33,7 +44,7 @@ public class AuthServiceTest {
 
 	@Test
 	public void testAuthConseiller3() {
-		String login = "ccastro";
+		String login = "rdupond";
 		String pwd = "1234";
 		boolean status;
 
@@ -41,5 +52,11 @@ public class AuthServiceTest {
 		status = servicePourTest.authConseiller(login, pwd);
 
 		Assert.assertFalse(status);
+	}
+	
+	@AfterClass
+	public static void apresClasse(){
+		ConseillerService service = new ConseillerService();
+		service.supprimerConseiller("rdupond");
 	}
 }
