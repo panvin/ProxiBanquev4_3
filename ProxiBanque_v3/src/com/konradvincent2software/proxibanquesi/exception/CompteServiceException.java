daca5c8b28a6a584dc8d233fb30ffde7403311e0
Vincent <panouilleres.vincent@yahoo.fr>
2016-09-13 10:21:14 +0200

    Classe Gestion d'exception pour CompteService

--- ProxiBanque_v3/src/com/konradvincent2software/proxibanquesi/exception/CompteServiceException.java
+++ ProxiBanque_v3/src/com/konradvincent2software/proxibanquesi/exception/CompteServiceException.java
@@ -1,18 +1,45 @@
 package com.konradvincent2software.proxibanquesi.exception;
 
+/**
+ * Classe d'exception personnalisée pour la classe CompteService
+ * Cette classe permet  de gérer les exceptions causées par CompteService
+ * @author Konrad THOMAS et VIncent PANOUILLERES
+ *
+ */
 public class CompteServiceException extends Exception {
 	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -2140652444662783112L;
 
+	/**
+	 * Constructeur par défaut de la classe 
+	 */
 	public CompteServiceException(){
 		super();
 	}
 	
+	/**
+	 * Constructeur de la classe prenant en compte les détails de l'erreur
+	 * @param details Les détails de l'erruer sous forme de chaine de caractere (String)
+	 */
 	public CompteServiceException(String details){
 		super(details);
+	}
+	
+	/**
+	 * Constructeur de la classe prenant en compte les détails de l'erreur et la cause de l'erreur
+	 * @param message Le message d'erruer (String)
+	 * @param cause La cause de l'erreur (Throwable)
+	 */
+	public CompteServiceException(String message, Throwable cause) { 
+		super(message, cause); 
+	}
+	
+	/**
+	 * Constructeur de la classe, prend en compte les cause de l'erreur
+	 * @param cause La cause de l'erreur (Throwable)
+	 */
+	public CompteServiceException(Throwable cause) {
+		super(cause);
 	}
 
 }
