ef9b33955191a7b0bb41f666d5a1638c9418aa7c
Konrad Thoams <konrad.thomas@aol.com>
2016-09-13 15:56:55 +0200

    Commentaires dans la ConseillerManagedBean ajoutés

--- ProxiBanque_v3/src/com/konradvincent2software/proxibanquesi/presentation/ConseillerManagedBean.java
+++ ProxiBanque_v3/src/com/konradvincent2software/proxibanquesi/presentation/ConseillerManagedBean.java
@@ -34,10 +34,10 @@ public class ConseillerManagedBean implements Serializable {
 	private String password;
 	private List<Client> clients;
 	private Client clientChoisi;
-//	private CompteCourant compteCourant;
-//	private CompteEpargne compteEpargne;
 	private List<Compte> comptes;
-
+	private String numeroCompteADebiter;
+	private String numeroCompteACrediter;
+	private String montantVirement;
 	
 	// Constructeurs
 	public ConseillerManagedBean() {
@@ -87,26 +87,39 @@ public class ConseillerManagedBean implements Serializable {
 	public void setClientChoisi(Client clientChoisi) {
 		this.clientChoisi = clientChoisi;
 	}	
-//	public CompteCourant getCompteCourant() {
-//		return compteCourant;
-//	}
-//	public void setCompteCourant(CompteCourant compteCourant) {
-//		this.compteCourant = compteCourant;
-//	}
-//	public CompteEpargne getCompteEpargne() {
-//		return compteEpargne;
-//	}
-//	public void setCompteEpargne(CompteEpargne compteEpargne) {
-//		this.compteEpargne = compteEpargne;
-//	}
 	public List<Compte> getComptes() {
 		return comptes;
 	}
 	public void setComptes(List<Compte> comptes) {
 		this.comptes = comptes;
 	}
+		public String getNumeroCompteADebiter() {
+		return numeroCompteADebiter;
+	}
+	public void setNumeroCompteADebiter(String numeroCompteADebiter) {
+		this.numeroCompteADebiter = numeroCompteADebiter;
+	}
+	public String getNumeroCompteACrediter() {
+		return numeroCompteACrediter;
+	}
+	public void setNumeroCompteACrediter(String numeroCompteACrediter) {
+		this.numeroCompteACrediter = numeroCompteACrediter;
+	}
+	public String getMontantVirement() {
+		return montantVirement;
+	}
+	public void setMontantVirement(String montantVirement) {
+		this.montantVirement = montantVirement;
+	}
 
 	// Autres méthodes
+	
+	
+	/**
+	 * Méthode pour authentifier un conseiller à partir de son identiant et son mot de passe. 
+	 * Si l'authentification réussit, la page acceuil.xhtml avec la liste de clients est affichée, 
+	 * sinon la page login.xhtml est renvoyée avec un messaged d'erreur. 
+	 */
 	public String authentifiaction() {
         if(as.authConseiller(login, password))
         {
@@ -124,16 +137,27 @@ public class ConseillerManagedBean implements Serializable {
         }
     }
 	
+	/**
+	 * Méthode qui permet de selectionner un client dans la liste des clients 
+	 * et qui renvoye dans la page modifier.xhtml qui permet des changer les données du client.
+	 * @param client
+	 */
 	public String selectionnerClient(Client client) {
 		setClientChoisi(client);
 		return "modifier";
 	}
 	
+	/**
+	 * Méthode qui éffectue la modification des données d'un client dans la base de données renvoyant sur la page acceuil.xhtml.
+	 */
 	public String modifierClient() {
 		cls.modifierClient(clientChoisi.getId(), clientChoisi);
 		return "acceuil";
 	}
 	
+	/**
+	 * Cette méthode crée la liste des comptes pour un client choisi et renvoye vers la pages comptes.xhtml.
+	 */
 	public String voirComptes(Client client) {
 		setClientChoisi(client);
 		Compte compteCourant = client.getCompteCourant();
@@ -147,8 +171,14 @@ public class ConseillerManagedBean implements Serializable {
 		return "comptes";
 	}
 	
-	public String deconnexion() {
-		return "login";
-	}
 	
+	/**
+	 * Cette méthode sert à effectuer un virement avec les données qui ont étées entrées dans la page virement.xhtml.
+	 * Elle n'est actuellement pas fonctionelle à cause de problèmes d'intégration qui n'ont pas pu être résolus avant la fin du projet.
+	 */
+	public String effectuerVirement() {
+		//System.out.println(numeroCompteACrediter);
+		//cms.virementCompteACompte(numeroCompteADebiter, numeroCompteACrediter, Float.parseFloat(montantVirement));
+		return "acceuil";
+	}
 }
